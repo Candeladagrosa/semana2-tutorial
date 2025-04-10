b752816319c2b1f8958d329df65381758d99b3fb
Tomi <tomasagustin.200358@gmail.com>
2025-04-10 16:46:27 -0300

    Agregar archivos de proyecto.

---
+++ semana2-tutorial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace semana2_tutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
